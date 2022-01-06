363df72bd23992caec98dfe2e50b3bae2eb81a83
Uneza Kashif <ezakashif@gmail.com>
2022-01-05 19:30:00 -0800

    First commit

---
+++ app/Http/Middleware/Teacher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+class Teacher
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role == 2) {
+            return redirect()->route('teacher');
+        }
+        return $next($request);
+    }
+}
